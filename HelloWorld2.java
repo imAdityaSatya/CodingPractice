500e1a3f2792d125a7f8537befcb4232b5e4038d
Aditya Satya <73811047+imAdityaSatya@users.noreply.github.com>
2022-03-04 22:06:26 +0530

    Add files via upload

---
+++ HelloWorld2.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld2 {
+	public static void sayHello() {
+		System.out.println("Hello World");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sayHello();
+	}
+
+}
