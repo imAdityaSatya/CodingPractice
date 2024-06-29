439ddcd36d2a6ab7f0aee3ce66236550c4496ce9
Aditya Satya <73811047+imAdityaSatya@users.noreply.github.com>
2024-06-29 18:34:24 +0530

    Update TwoSum.java

--- Array/TwoSum.java
+++ Array/TwoSum.java
@@ -4,36 +4,32 @@ import java.util.Map;
 /**
  * Problem 1 : Two Sum
  * 
- * Problem Statement: Given an array of integers arr and an integer target.
- * Return the indices of the two numbers such that they add up to give the target.
+ * Given an array of integers 'arr' and an integer 'target'. Return the indices of two numbers such that they sum up to target.
+ * Assume that each input would have exactly one solution, and do not use the same element twice.
  * 
  * Sample Input: arr = [2,7,11,15], target = 9
  * Sample Output: [0,1]
  */
 
-public class Problem01 {
+public class TwoSum {
     
-    public static void function1(int arr[], int target){
-        // int res[] = new int[2];
+    public static void bruteFunction1(int arr[], int target){
         int numOne=0, numTwo=0;
         for(int i=0; i<arr.length; i++){
             for(int j=i+1; j<arr.length; j++){
                 if(target == arr[i]+arr[j]){
-                    // res = new int[]{i,j};
                     numOne = i;
                     numTwo = j;
                 }
             }
         }
-        // res[0]= numOne;
-        // res[1]= numTwo;
         int res[] = {numOne, numTwo};
         for(int i=0; i<2; i++){
             System.out.print(res[i]+" ");
         }
     } 
 
-    public static int[] function2(int arr[], int target){
+    public static int[] bruteFunction2(int arr[], int target){
         int res[] = new int[2];
         for(int i=0; i<arr.length; i++){
             for(int j=i+1; j<arr.length; j++){
@@ -46,7 +42,7 @@ public class Problem01 {
         return res;
     } 
 
-    public static int[] function3(int[] arr, int target) {
+    public static int[] optFunction(int[] arr, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i<arr.length; i++) {
             int complement = target - arr[i];
@@ -62,16 +58,19 @@ public class Problem01 {
         int arr[] = {2,7,11,15};
         int target = 9;
         
-        function1(arr, target);
+        // bruteFunction1(arr, target);
 
-        // int ans[] = function2(arr, target);
+        // System.out.println(bruteFunction2(arr, target));    // printing [I@5acf9800
+
+        // int ans[] = bruteFunction2(arr, target);
         // for(int i=0; i<2; i++){
         //     System.out.print(ans[i]+" ");
         // }
         
-        // System.out.println(function2(arr, target));     // printing [I@5acf9800
-
-        // System.out.println(function3(arr, target));
+        int ans[] = optFunction(arr, target);
+        for(int i=0; i<2; i++){
+            System.out.print(ans[i]+" ");
+        }
 
     }
 
