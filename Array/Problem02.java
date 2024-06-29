f97d3e47fdf880159975f36f225a1cb7cc87340f
Aditya Satya <73811047+imAdityaSatya@users.noreply.github.com>
2024-06-29 17:06:36 +0530

    Update Problem02.java

--- Array/Problem02.java
+++ Array/Problem02.java
@@ -3,6 +3,7 @@
  * 
  * You are given an array height where height[i] represents the height of a vertical line on the i-th position. 
  * Find two lines, which together with the x-axis forms a container, such that the container contains the most water.
+ *
  * Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
  * Find two lines, which together with the x-axis forms a container, such that the container contains the most water.
  * 
