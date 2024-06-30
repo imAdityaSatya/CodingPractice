dacbcbba84ff83fc1e946dc64dce4423a1bd86ae
Aditya Satya <73811047+imAdityaSatya@users.noreply.github.com>
2024-06-30 22:40:59 +0530

    Update Problem02.java

--- Array/Problem02.java
+++ Array/Problem02.java
@@ -1,19 +1,16 @@
 /*
  * Problem 2 : Container With Most Water
  * 
- * You are given an array height where height[i] represents the height of a vertical line on the i-th position. 
- * Find two lines, which together with the x-axis forms a container, such that the container contains the most water.
- *
- * Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
+ * Given an array 'height' where height[i] represents the height of a vertical line on the i-th position. 
  * Find two lines, which together with the x-axis forms a container, such that the container contains the most water.
  * 
  * Input: height = [1,8,6,2,5,4,8,3,7]
  * Output: 49
- * Explanation: The two lines with height 8 and 7 together form the container with the most water, which can hold 49 units of water.
+ * Explanation: The 2 lines with heights 8 and 7 together form the container with the most water, holding 49 water units.
  */
 
-public class Problem02 {
-    public static int containerWithMostWater(int[] height) {
+public class ContainerMostWater {
+    public static int bruteFunction(int[] height) {
         int maxArea = 0;
         int n = height.length;
         for (int i = 0; i < n; i++) {
@@ -25,8 +22,8 @@ public class Problem02 {
         return maxArea;
     }
 
-    public static void main(String[] args){
+    public static void main(String args[]){
         int[] height = {1,8,6,2,5,4,8,3,7};
-        System.out.println(containerWithMostWater(height));
+        System.out.println(bruteFunction(height));
     }
 }
